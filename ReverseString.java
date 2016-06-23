001c272b7d09bc815748a029c4c7bfc865cab797
tangyeah <tangyeah295@163.com>
2016-06-23 04:17:05 -0400

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,14 +1,14 @@
 public class Solution {
 	
 	public static String reverseString(String s) {
-        int len = s.length();
-	    StringBuilder sb = new StringBuilder();
-    	for(int i=len; i>0; i--)
-	    	sb.append(s.charAt(i-1));
+		int len = s.length();
+	   	StringBuilder sb = new StringBuilder();
+   		for(int i=len; i>0; i--)
+   			sb.append(s.charAt(i-1));
     	return sb.toString();
     }
 	
-    public static void main(String[] args) {
-        System.out.println(reverseString("hello!"));
-    }
+	public static void main(String[] args) {
+       	System.out.println(reverseString("hello!"));
+	}
 }
